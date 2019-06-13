36ee20b3912dba156ab6d9ef8cf8d7e4e63aa3cf
Aftab Shaikh <aftabshk@In-Aftabshk.local>
2019-06-13 20:23:28 +0530

    Created the board and applied click for selecting current generation

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,34 @@
-import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { TestBed, async } from "@angular/core/testing";
+import { AppComponent } from "./app.component";
 
-describe('AppComponent', () => {
+describe("AppComponent", () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent]
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
+  it("should create the app", () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'gameOfLife'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('gameOfLife');
-  });
+  // it("should render title in a h1 tag", () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   fixture.detectChanges();
+  //   const compiled = fixture.debugElement.nativeElement;
+  //   expect(compiled.querySelector("h1").textContent).toContain(
+  //     "Welcome to gameOfLife!"
+  //   );
+  // });
 
-  it('should render title in a h1 tag', () => {
+  it("should call select cell when table has been clicked", () => {
     const fixture = TestBed.createComponent(AppComponent);
+    const component = fixture.componentInstance;
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to gameOfLife!');
+    const cell = fixture.debugElement.nativeElement.querySelector("[id='1,2']");
+    cell.click();
+    expect(component.currentGeneration).toContain([1, 2]);
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { extend } from "webdriver-js-extender";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.less"]
 })
-export class AppComponent {
-  title = 'gameOfLife';
+export class AppComponent implements OnInit {
+  currentGeneration: Number[][];
+
+  createArray(count: number) {
+    return new Array(count);
+  }
+
+  ngOnInit() {
+    this.currentGeneration = [];
+  }
+
+  selectCell(event) {
+    const coordinates = (event.srcElement as Element).id
+      .split(",")
+      .map(this.convertToNumber);
+    this.currentGeneration.push(coordinates);
+  }
+
+  isAlive(row, column) {
+    return this.currentGeneration.some(
+      coordinate => coordinate[0] === +row && coordinate[1] === +column
+    );
+  }
+
+  private convertToNumber(value: string): Number {
+    return +value;
+  }
 }
